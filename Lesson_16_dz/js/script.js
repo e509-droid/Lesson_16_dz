28dff91ab55020ae1068196e65c3a2706a5ce0ee
e509-droid <77908442+e509-droid@users.noreply.github.com>
2024-12-16 22:57:17 +0100

    Add files via upload

---
+++ Lesson_16_dz/js/script.js
@@ -0,0 +1,27 @@
+const dayOfWeek = 1;
+
+switch (dayOfWeek) {
+  case 1:
+    console.log("Понедельник");
+    break;
+  case 2:
+    console.log("Вторник");
+    break;
+  case 3:
+    console.log("Среда");
+    break;
+  case 4:
+    console.log("Четверг");
+    break;
+  case 5:
+    console.log("Пятница");
+    break;
+  case 6:
+    console.log("Суббота");
+    break;
+  case 7:
+    console.log("Воскресенье");
+    break;
+  default:
+    console.log("Ошибка: Введите число от 1 до 7.");
+}
